d9d4725b1eae7e7d2174a7562cb5c931510587dc
brandonrexbrown@email.arizona.edu <redsoxmtg@yahoo.com>
2014-04-11 10:30:05 -0700

    status effects negro

---
+++ HelixIndustriesPokemonTowerDefenseV1/src/model/effect.java
@@ -0,0 +1,38 @@
+package model;
+
+public enum effect {
+	poison, paralyze, sleep, burn, freeze, confuse;
+	
+	public void getStatuseffect(effect status, double speed, int DoT, int length){
+		if(status == poison){
+			speed = 0.75;
+			DoT = 5;
+			length = 10000000;
+		}
+		if(status == paralyze){
+			speed = 0;
+			DoT = 1;
+			length = 2;
+		}
+		if(status == sleep){
+			speed = 0;
+			DoT = 0;
+			length = 5;
+		}
+		if(status == burn){
+			speed = 2;
+			DoT = 15;
+			length = 5;
+		}
+		if(status == freeze){
+			speed = 0.25;
+			DoT = 2;
+			length = 5;
+		}
+		if(status == confuse){
+			speed = -1;
+			DoT = 0;
+			length = 2;
+		}
+	}
+}
