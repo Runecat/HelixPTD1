ce225fcdbbe7c9636bdd796cd85cc5413c5fd748
Runecat <danielsluces@email.arizona.edu>
2014-04-15 09:21:49 -0700

    Updated much of the Tower and Attack logic

--- HelixIndustriesPokemonTowerDefenseV1/src/model/Game.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/model/Game.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Game {
+import java.util.ArrayList;
+import towers.*;
 
+public class Game 
+{
+	ArrayList<Tower> towers;
+	ArrayList<Mob> mobs;
+	Map map;
+//figures out which mobs are affected by which towers, contains a map
 }
