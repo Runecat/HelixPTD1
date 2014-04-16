c7e6ef6d9dfe5ebf2b0f646a1212c18b4f30fd37
Will Clement <William.w.clement@gmail.com>
2014-04-16 15:07:24 -0700

    MenuPanel!

--- HelixIndustriesPokemonTowerDefenseV1/src/view/PTDGUI.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/view/PTDGUI.java
@@ -1,9 +1,14 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import model.Game;
 
@@ -14,6 +19,7 @@ public class PTDGUI extends JFrame {
 	 * 
 	 */
 	
+	BackgroundFrame background;
 	
 	public static void main(String[] args) {
 		Game theGame = new Game();
@@ -23,24 +29,56 @@ public class PTDGUI extends JFrame {
 	public PTDGUI(Game game) {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(1000, 720);
-		this.setLayout(new BorderLayout());
 		this.setTitle("Helix PTD Pre-Alpha 0.1");
+		
+		background = new BackgroundFrame();
+		background.setLayout(new BorderLayout());
+		this.add(background);
 		
 		MapPanel mapPanel = new MapPanel(game);
 		mapPanel.setPreferredSize(new Dimension(800, 720));
-		this.add(mapPanel, BorderLayout.LINE_START);
+		background.add(mapPanel, BorderLayout.LINE_START);
 				
 		MenuPanel menu = new MenuPanel();
 		menu.setPreferredSize(new Dimension(184, 720));
-		this.add(menu, BorderLayout.LINE_END);
+		background.add(menu, BorderLayout.LINE_END);
 		
-		
+
 		
 		
 		this.setVisible(true);
 		
 		
 
+	}
+	
+	
+	
+	private class BackgroundFrame extends JPanel {
+		ImageLoader loader = new ImageLoader();
+		BufferedImage grass;
+		
+		public BackgroundFrame() {
+			try {
+				grass = loader.loadImage("Images/GrassIsForMowin.png");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			for (int i = 0; i <= this.getWidth()/grass.getWidth(); i++) {
+				for (int j = 0; j <= this.getHeight()/grass.getHeight(); j++){
+					
+					g.drawImage(grass, i*grass.getWidth(), j*grass.getHeight(), null);
+					
+				}
+			}
+			
+		}
 	}
 	
 }
