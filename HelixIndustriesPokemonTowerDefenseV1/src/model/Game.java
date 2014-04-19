7df8d4df4215bab5eeb1a95ea7cb3c43277c7574
Runecat <danielsluces@email.arizona.edu>
2014-04-19 13:34:49 -0700

    Did some work on the game class

--- HelixIndustriesPokemonTowerDefenseV1/src/model/Game.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/model/Game.java
@@ -16,9 +16,23 @@ public class Game extends Observable {
 	// game at hand?
 	
 	private List<Player> players = new ArrayList<Player>(MAX_PLAYERS); // A list of players. yup.
+	//private ArrayList<Mob> mobs;
+	//private ArrayList<Tower> towers;
 	
 	public Game() {	// added constructor.
 		// add observers and other things.
+	}
+	
+	public static void main(String args[])
+	{
+		ArrayList<Tower> towers = null;
+		ArrayList<Mob> mobs = null;
+		for(Tower t:towers)
+		{
+			//do this
+		}
+		//Set up listeners for placing towers, 
+		//Set up Timer and listeners for tower attacks.
 	}
 	
 	public Map getMap() {	// added this
@@ -41,7 +55,54 @@ public class Game extends Observable {
 	{
 		ArrayList<Mob> mobs = null;
 		ArrayList<Tile> tilesInRange = null;
+		Tile[][] grid = currentMap.getGrid();
+		//grab which tiles are in range
+		//for those tiles in range, find mobs on them
+		//have tower attack those mobs if permitted to
+		int xMin = t.getColumn() - a.getHorizontalRange();
+		int xMax = t.getColumn() + a.getHorizontalRange();
+		int yMin = t.getRow() - a.getVerticalRange();
+		int yMax = t.getRow() - a.getVerticalRange();
 		
+		int x;
+		int y;
+		
+		if(xMin <0)
+		{
+			xMin = 0;
+		}
+		
+		if(yMin < 0)
+		{
+			yMin = 0;
+		}
+		
+		if(xMax > currentMap.getWidth())
+		{
+			xMax = currentMap.getWidth();
+		}
+		
+		if(yMax > currentMap.getHeight())
+		{
+			yMax = currentMap.getHeight();
+		}
+		//possibly need to add -1 to the max....
+		
+		
+		for(x = xMin;x<xMax;x++)
+		{
+			for(y = yMin;y<yMax;y++)
+			{
+				if(grid[x][y].hasMob())
+				{
+					tilesInRange.add(grid[x][y]);
+				}
+			}
+		}
+		
+		mobs.add(tilesInRange.get(0).getMobs().get(0));//for now, just attack first mob in first tile
+		
+		//depending on type, add from all possible mobs the only mobs that will attack
 		return mobs;
 	}
 	
