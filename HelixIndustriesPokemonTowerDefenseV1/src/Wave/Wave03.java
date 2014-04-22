c57fce0da507bd9232c27bc9c3ab4b51f20ba714
brandonrexbrown@email.arizona.edu <redsoxmtg@yahoo.com>
2014-04-21 18:49:25 -0700

    made wave abstract

--- HelixIndustriesPokemonTowerDefenseV1/src/Wave/Wave03.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/Wave/Wave03.java
@@ -8,22 +8,22 @@ import Mob.Pidgey;
 import Mob.Rattata;
 import Mob.Weedle;
 
-public class Wave03 {
-	private Wave wave3 = new Wave();
+public class Wave03 extends Wave{
 	private Mob mob;
 		
 	public Wave03(){
+		super();
 		for(int i = 0; i < 25; i++){
 			if(i < 15)
 				if(i % 2 == 0)
-					wave3.add(new Rattata());
+					add(new Rattata());
 				else
-					wave3.add(new Pidgey());
+					add(new Pidgey());
 			else
 				if(i % 2 == 0)
-					wave3.add(new Weedle());
+					add(new Weedle());
 				else
-					wave3.add(new Caterpie());
+					add(new Caterpie());
 		}
 	}
 }
--- HelixIndustriesPokemonTowerDefenseV1/src/Wave/Wave04.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/Wave/Wave04.java
@@ -12,33 +12,33 @@ import Mob.Sandshrew;
 import Mob.Spearow;
 import Mob.Weedle;
 
-public class Wave04 {
+public class Wave04 extends Wave{
 
-	private Wave wave4 = new Wave();
 	private Mob mob;
 		
 	public Wave04(){
+		super();
 		for(int i = 0; i < 35; i++){
 			if(i < 5){
 				if(i % 2 == 0)
-					wave4.add(new Rattata());
+					add(new Rattata());
 				else
-					wave4.add(new Pidgey());
+					add(new Pidgey());
 			}
 			else if (i > 5 && i < 30){
 				if(i % 2 == 0)
-					wave4.add(new Pidgey());
+					add(new Pidgey());
 				else
-					wave4.add(new Spearow());
+					add(new Spearow());
 			}
 			else if(i > 30 && i < 35){
 				if(i % 2 == 0)
-					wave4.add(new Mankey());
+					add(new Mankey());
 				else
-					wave4.add(new Sandshrew());
+					add(new Sandshrew());
 			}
 			else
-				wave4.add(new Jigglypuff());
+				add(new Jigglypuff());
 		}
 	}
 }
