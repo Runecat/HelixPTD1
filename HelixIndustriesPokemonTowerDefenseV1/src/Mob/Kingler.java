9abe183bf30781310d4b385a63d6b56be9415793
brandonrexbrown@email.arizona.edu <redsoxmtg@yahoo.com>
2014-05-11 23:08:10 -0700

    all mobs and waves and shit

---
+++ HelixIndustriesPokemonTowerDefenseV1/src/Mob/Kingler.java
@@ -0,0 +1,18 @@
+package Mob;
+
+import java.awt.image.BufferedImage;
+
+import model.Type;
+
+public class Kingler extends Mob{
+
+	public Kingler(BufferedImage b) {
+		super(b);
+		super.name = "Kingler";
+		super.health = 100;
+		super.moveSpeed = 2;
+		super.type = Type.WATER;
+		super.money = 8;
+	}
+
+}
