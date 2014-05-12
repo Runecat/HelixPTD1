b3c610950ee9cf140bdb7883265c87830842a858
brandonrexbrown@email.arizona.edu <redsoxmtg@yahoo.com>
2014-05-12 16:09:15 -0700

    added to map

--- HelixIndustriesPokemonTowerDefenseV1/src/maps/Level1.java
+++ HelixIndustriesPokemonTowerDefenseV1/src/maps/Level1.java
@@ -63,7 +63,7 @@ public class Level1 extends Map {
 		waves.add(new Wave03());
 		
 		// spawner will be declared for each map specifically.
-		Spawner spawn = new Spawner(grid[3][3], waves);
+		Spawner spawn = new Spawner(grid[3][0], waves);
 		super.setSpawner(spawn);
 				
 		// TRACK NIGGA (defined specifically in each map)
@@ -77,9 +77,61 @@ public class Level1 extends Map {
 		grid[3][7].setNextPathTile(grid[3][8]);
 		grid[3][8].setNextPathTile(grid[3][9]);
 		grid[3][9].setNextPathTile(grid[3][10]);
-		grid[3][10].setNextPathTile(grid[4][10]);
-		grid[4][10].setNextPathTile(grid[5][10]);
-		grid[5][10].setNextPathTile(grid[5][11]);
+		grid[3][10].setNextPathTile(grid[3][11]);
+		grid[3][11].setNextPathTile(grid[3][12]);
+		grid[3][12].setNextPathTile(grid[3][13]);
+		grid[3][13].setNextPathTile(grid[3][14]);
+		grid[3][14].setNextPathTile(grid[3][15]);
+		grid[3][15].setNextPathTile(grid[3][16]);
+		grid[3][16].setNextPathTile(grid[3][17]);
+		grid[3][17].setNextPathTile(grid[3][18]);
+		grid[3][18].setNextPathTile(grid[3][19]);
+		grid[3][19].setNextPathTile(grid[3][20]);
+		grid[3][20].setNextPathTile(grid[3][21]);
+		grid[3][21].setNextPathTile(grid[3][22]);
+		grid[3][22].setNextPathTile(grid[3][23]);
+		
+		grid[3][23].setNextPathTile(grid[4][23]);
+		grid[4][23].setNextPathTile(grid[4][24]);
+		
+		grid[4][24].setNextPathTile(grid[5][24]);
+		grid[5][24].setNextPathTile(grid[6][24]);
+		grid[6][24].setNextPathTile(grid[7][24]);
+		grid[7][24].setNextPathTile(grid[8][24]);
+		grid[8][24].setNextPathTile(grid[9][24]);
+		grid[9][24].setNextPathTile(grid[10][24]);
+		grid[10][24].setNextPathTile(grid[11][24]);
+		grid[11][24].setNextPathTile(grid[12][24]);
+		grid[12][24].setNextPathTile(grid[13][24]);
+		grid[13][24].setNextPathTile(grid[14][24]);
+		grid[14][24].setNextPathTile(grid[15][24]);
+		
+		grid[15][24].setNextPathTile(grid[15][23]);
+		grid[15][23].setNextPathTile(grid[16][23]);
+		
+		grid[16][23].setNextPathTile(grid[16][22]);
+		grid[16][22].setNextPathTile(grid[16][21]);
+		grid[16][21].setNextPathTile(grid[16][20]);
+		grid[16][20].setNextPathTile(grid[16][19]);
+		grid[16][19].setNextPathTile(grid[16][18]);
+		grid[16][18].setNextPathTile(grid[16][17]);
+		grid[16][17].setNextPathTile(grid[16][16]);
+		grid[16][16].setNextPathTile(grid[16][15]);
+		grid[16][15].setNextPathTile(grid[16][14]);
+		grid[15][14].setNextPathTile(grid[16][13]);
+		grid[15][13].setNextPathTile(grid[16][12]);
+		grid[16][12].setNextPathTile(grid[16][11]);
+		grid[16][11].setNextPathTile(grid[16][10]);
+		grid[16][10].setNextPathTile(grid[16][9]);
+		grid[16][9].setNextPathTile(grid[16][8]);
+		grid[16][8].setNextPathTile(grid[16][7]);
+		grid[16][7].setNextPathTile(grid[16][6]);
+		grid[16][6].setNextPathTile(grid[16][5]);
+		grid[16][5].setNextPathTile(grid[16][4]);
+		grid[16][4].setNextPathTile(grid[16][3]);
+		grid[16][3].setNextPathTile(grid[16][2]);
+		grid[16][2].setNextPathTile(grid[16][1]);
+		grid[16][1].setNextPathTile(grid[16][0]);
 		super.buildPath();
 		super.setLevels(waves.size());
 		
